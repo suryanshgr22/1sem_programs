55b8f0e4c3b39e319afa942ca3e7aeb7b50b053c
Suryansh <34952630+suryanshgr22@users.noreply.github.com>
2023-01-13 19:56:12 +0530

    Create SizeofOperator.c

---
+++ SizeofOperator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a = 5;
+    char ch = 'X';    // Capital X
+    printf("Size of a : %d Bytes\n" ,sizeof(a));
+    printf("Size of X : %d Bytes\n",sizeof(ch));
+    return 0;
+
+}
